06a9d2d086205a61e36fa8503468b368b254635d
azizHr <azizharkati20@gmail.com>
2024-03-23 17:19:58 +0000

    first-commit

---
+++ src/test/java/com/example/challengetechnique/ChallengeTechniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengetechnique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTechniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
