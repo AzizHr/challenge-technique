92896c0181211680e66a184ade84c0599a131c37
azizHr <azizharkati20@gmail.com>
2024-03-23 17:31:38 +0000

    create User entity

---
+++ src/main/java/com/example/challengetechnique/models/User.java
@@ -0,0 +1,18 @@
+package com.example.challengetechnique.models;
+
+import java.time.LocalDate;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String city;
+    private String country;
+    private String avatar;
+    private String company;
+    private String jobPosition;
+    private String mobile;
+    private String email;
+    private String username;
+    private String password;
+}
