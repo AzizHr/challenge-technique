8c314568fd9cf180d0f65c4663cb99061d49a29a
azizHr <azizharkati20@gmail.com>
2024-03-23 17:44:49 +0000

    create FakeUserGenerator helper

---
+++ src/main/java/com/example/challengetechnique/utils/FakeUserGenerator.java
@@ -0,0 +1,14 @@
+package com.example.challengetechnique.utils;
+
+import com.example.challengetechnique.models.User;
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class FakeUserGenerator {
+    private final Faker faker;
+
+    public void generate(User user) {
+
+    }
+}
