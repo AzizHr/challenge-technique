634c84cd79ec8540d6ceb515210fb6df5cad53c1
azizHr <azizharkati20@gmail.com>
2024-03-23 17:35:56 +0000

    add required annotations for User entity

--- src/main/java/com/example/challengetechnique/models/User.java
+++ src/main/java/com/example/challengetechnique/models/User.java
@@ -1,10 +1,24 @@
 package com.example.challengetechnique.models;
 
 import com.example.challengetechnique.enums.Role;
-
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
+import java.util.UUID;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
@@ -17,5 +31,6 @@ public class User {
     private String email;
     private String username;
     private String password;
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
